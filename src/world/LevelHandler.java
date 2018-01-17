999a30f4e4df48d4b5ffcca41e103d7e7e4c9761
Harry Zhang <zh.harry@yahoo.ca>
2018-01-16 19:31:47 -0500

    Activator Collide

--- src/world/LevelHandler.java
+++ src/world/LevelHandler.java
@@ -9,16 +9,28 @@ public abstract class LevelHandler {
 	public WorldServer world;
 	public Level level;
 
-	public void activator(String activator, ArrayList<EntityPlayer> playerList) {
-		
+	// Activators are named: Activator.ID[.RequiredPlayers]
+	public void activator(String name, ArrayList<EntityPlayer> playerList) {
+		String[] args = name.split("\\.");
+		int id = Integer.parseInt(args[1]), colPlayers = args.length <= 2 ? 1 : Integer.parseInt(args[2]);
+		if (playerList.size() > 0) {
+			if (playerList.size() >= colPlayers) {
+				level.setCollidable("Collider." + id, false);
+				level.setRenderable("Collider." + id, false);
+			} else {
+				for (EntityPlayer player: playerList) {
+				}
+			}
+		} else {
+			level.setCollidable("Collider." + id, true);
+			level.setRenderable("Collider." + id, true);
+		}
 	}
-
-	public void finishLevel(String activator, ArrayList<EntityPlayer> playerList) {
-		
-	}
-
-	public void collideOther(String object, EntityPlayer player) {
-		
-	}
+	
+	public abstract void onPlayerJoin();
+	
+	public abstract void onPlayerLeave();
+	
+	public abstract void collideOther(String object, EntityPlayer player);
 	
 }
