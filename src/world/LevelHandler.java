f66c96460bf720b4ccd4dc629bf0cb3b2e1ca84c
Harry Zhang <zh.harry@yahoo.ca>
2018-01-16 19:55:08 -0500

    Finished LevelHandler

--- src/world/LevelHandler.java
+++ src/world/LevelHandler.java
@@ -1,37 +1,54 @@
 package world;
 
-import entity.EntityPlayer;
-
 import java.util.ArrayList;
+
+import entity.EntityPlayer;
 
 public abstract class LevelHandler {
 
 	public WorldServer world;
 	public Level level;
 
-	// Activators are named: Activator.ID[.RequiredPlayers]
+	// Activators are named: Activator.ID[.RequiredPlayers] or Activator.Exit
 	public void activator(String name, ArrayList<EntityPlayer> playerList) {
 		String[] args = name.split("\\.");
-		int id = Integer.parseInt(args[1]), colPlayers = args.length <= 2 ? 1 : Integer.parseInt(args[2]);
-		if (playerList.size() > 0) {
-			if (playerList.size() >= colPlayers) {
-				level.setCollidable("Collider." + id, false);
-				level.setRenderable("Collider." + id, false);
-			} else {
-				for (EntityPlayer player: playerList) {
-					player.sendMessage(colPlayers - playerList.size() + " more player(s) required!");
-				}
-			}
+
+		// Move Player to the next level
+		if (name.startsWith("Activator.Exit")) {
+			String nextLevel = args[2];
+			for (EntityPlayer player : playerList)
+				world.setEntityLevel(player, nextLevel);
+
+			// Trigger a activator
 		} else {
-			level.setCollidable("Collider." + id, true);
-			level.setRenderable("Collider." + id, true);
+			int id = Integer.parseInt(args[1]), colPlayers = args.length <= 2 ? 1 : Integer.parseInt(args[2]);
+			if (playerList.size() > 0) {
+				// Disable corresponding collider
+				if (playerList.size() >= colPlayers) {
+					level.setCollidable("Collider." + id, false);
+					level.setRenderable("Collider." + id, false);
+					// Notify players remaining required players
+				} else
+					for (EntityPlayer player : playerList)
+						player.sendMessage(colPlayers - playerList.size() + " more player(s) required!");
+				// Enable corresponding collider
+			} else {
+				level.setCollidable("Collider." + id, true);
+				level.setRenderable("Collider." + id, true);
+			}
 		}
 	}
-	
-	public abstract void onPlayerJoin();
-	
-	public abstract void onPlayerLeave();
-	
-	public abstract void collideOther(String object, EntityPlayer player);
-	
+
+	public void onPlayerJoin(EntityPlayer player) {
+		String[] levelData = level.level.split("_");
+		String levelID = levelData[0].substring(5);
+		String levelDesc = levelData[1];
+		player.sendMessage("Level " + levelID + ": " + levelDesc);
+	}
+
+	public void collideOther(String object, ArrayList<EntityPlayer> playerList) {
+		for (EntityPlayer player : playerList) 
+			player.teleportTo("Marker.Spawn");
+	}
+
 }
